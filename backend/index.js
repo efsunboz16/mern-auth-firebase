bb8166f5f16449e43aceebed764f44d76b556167
efsunboz16 <devrim.boz16@gmail.com>
2024-07-30 16:44:01 +0300

    complete sign up page functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/userroute.js'
 import authRoutes from './routes/authroute.js'
 dotenv.config()
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB')
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,25 +1,66 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+    const [formData, setFromData] = useState({});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = (e) => {
+        setFromData({ ...formData, [e.target.id]: e.target.value });
+    }
+    console.log(formData);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            setError(false);
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            console.log(data);
+            setLoading(false);
+            if (data.success === false) {
+                setError(true);
+                return;
+            }
+
+        } catch (error) {
+            setLoading(false);
+            setError(true);
+        }
+    }
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-            <form className='flex flex-col gap-4'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
                 <input type="text" placeholder='Username'
-                    id='username' className='bg-slate-100 p-3 rounded-lg' />
+                    id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
                 <input type="email" placeholder='Email'
-                    id='email' className='bg-slate-100 p-3 rounded-lg' />
+                    id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
                 <input type="password" placeholder='Password'
-                    id='password' className='bg-slate-100 p-3 rounded-lg' />
-                <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+                    id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+                    {loading ? 'Loading...' : 'Sign up'}
+                </button>
             </form>
             <div className='flex justify-between'>
                 <p>Have an account?</p>
                 <Link to='/sign-in'>
-                    <span className='text-blue-500'>Sign in</span>
+                    <span className='text-blue-500'>
+                        Sign in
+                    </span>
                 </Link>
             </div>
+            <p className='text-red-600'>{error && 'Something went wrong'}</p>
         </div>
     )
 }
