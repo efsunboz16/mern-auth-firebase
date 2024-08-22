465e9c1c7d95a8a3ff48380040aa73a86944333b
efsunboz16 <devrim.boz16@gmail.com>
2024-08-22 16:41:05 +0300

    complete image upload functionality using firebase storage

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,18 +1,74 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage'
+import { app } from '../../firebase';
 
 
 const Profile = () => {
 
+    const fileRef = useRef(null)
+    const [image, setImage] = useState(undefined);
+    const [imagePercent, setImagePercent] = useState(0);
+    const [imageError, setImageError] = useState(false);
+    const [formData, setFormData] = useState({})
+
     const { currentUser } = useSelector((state) => state.user);
+
+    useEffect(() => {
+        if (image) {
+            handleFileUpload(image)
+        }
+    }, [image])
+
+    const handleFileUpload = async (image) => {
+        const storage = getStorage(app)
+        const fileName = new Date().getTime() + image.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setImagePercent(Math.round(progress))
+            });
+        (error) => {
+            setImageError(true)
+        }
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(
+                (downloadURL) => {
+                    setFormData({ ...formData, profilePicture: downloadURL });
+                }
+            )
+        }
+
+
+    }
 
     return (
         <div className='p-3 max-w-lg mx-auto flex flex-col items-center'>
             <h1 className='text-3xl font-semibold text-center my-7'>PROFILE</h1>
             <form action="" className='flex flex-col w-5/6 gap-7'>
+                <input type="file" ref={fileRef} className='hidden' accept='image/*' onChange={(e) => setImage(e.target.files[0])} />
                 <img src={currentUser.profilePicture} alt="profile"
-                    className='self-center rounded-full cursor-pointer object-cover' />
+                    className='self-center rounded-full cursor-pointer object-cover' onClick={() => fileRef.current.click()} />
+
+                <p className='flex justify-center flex-row w-full'>
+                    {imageError ? (
+                        <span>
+                            Error upoloading image
+                        </span>
+                    ) : imagePercent > 0 && imagePercent < 100 ? (
+                        <span>
+                            {`Uploading: ${imagePercent} %`}
+                        </span>
+                    ) : imagePercent === 100 ? (
+                        <span className='text-green-500 '>
+                            Image uploaded successfully
+                        </span>
+                    ) : ''}
+                </p>
 
                 <input type="text"
                     defaultValue={currentUser.username} id='username' placeholder='Username' className='bg-slate-100 p-3 rounded-lg' />
